68a6df0b273aff79bcf262598f699022974b862f
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-05-15 20:19:11 +0200

    Paint grid items on mouseenter

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,17 @@ const GRID_SIZE = 16 * 16;
 const container = document.querySelector("#container");
 
 for(let i = 0; i < GRID_SIZE; i++) {
+    div = setUpGridItem();
+    container.appendChild(div);
+}
+
+function setUpGridItem() {
     const div = document.createElement("div");
     div.classList.add("grid-item");
-    container.appendChild(div);
+
+    div.addEventListener("mouseenter", () =>{
+        div.style.backgroundColor = "black";
+    })
+
+    return div;
 }
